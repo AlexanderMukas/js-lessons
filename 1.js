81d11575861015a35ffa3d386b09a2b6954b0d1d
AlexanderMukas <alexandermukas@yahoo.com>
2020-08-20 23:42:51 +0300

    Add first lessson

---
+++ 1.js
@@ -0,0 +1,11 @@
+// First lesson - types of variables and loops
+var someVar = 5;
+let secondVar = 'Hello';
+
+// constanta - not use twice
+const arr = ['Alexander', 'Peter'];
+console.log(`${secondVar} ${arr[0]} and ${arr[1]}!`);
+
+arr = ['John', 'Elisabeth'];
+
+console.log(`${secondVar} ${arr[0]} and ${arr[1]}!`);
