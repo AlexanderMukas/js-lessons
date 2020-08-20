ead99920cf844b3acd4fee6ec7cf868f7c6d86fc
AlexanderMukas <alexandermukas@yahoo.com>
2020-08-20 23:56:14 +0300

    Some commit

--- 1.js
+++ 1.js
@@ -6,6 +6,24 @@ let secondVar = 'Hello';
 const arr = ['Alexander', 'Peter'];
 console.log(`${secondVar} ${arr[0]} and ${arr[1]}!`);
 
-arr = ['John', 'Elisabeth'];
+//arr = ['John', 'Elisabeth'];
+//console.log(`${secondVar} ${arr[0]} and ${arr[1]}!`);
+// Arrays and Objects
+let myArr = [1,2,3,4,5,6,7,8,9];
 
-console.log(`${secondVar} ${arr[0]} and ${arr[1]}!`);
+// print every item of list/array
+myArr.forEach( item => console.log(item));
+myArr.forEach( item => console.log(item*10));
+// methods of List
+console.log( myArr.length )
+// Pop - delete last item
+// push - add item in end of arr
+myArr.pop();
+myArr.push(666);
+console.log(myArr);
+
+let myObj = {key1: "This is ", key2: "JavaScript"};
+//add new keys
+myObj.key3 = 'Some else';
+myObj.key4 = 'Some else again';
+console.log(myObj);
