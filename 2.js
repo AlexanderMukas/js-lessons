73c4791c98d95166475b2548a39eca74018284f4
AlexanderMukas <alexandermukas@yahoo.com>
2020-08-24 23:07:08 +0300

    Add 2 lesson file

---
+++ 2.js
@@ -0,0 +1,14 @@
+// function
+
+function myFunc(Obj){
+    Obj.brand = "Toyota";
+}
+
+/* new var myCar 
+*/
+let myCar = {
+    brand: "BMW",
+    model: "340d",
+    year: 1995
+};
+console.log(`My car is - ${myCar.brand} ${myCar.model} `);
