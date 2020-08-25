e3b35040df87380e1cb1b3212565886a196acbfb
AlexanderMukas <alexandermukas@yahoo.com>
2020-08-26 01:44:19 +0300

    Functions Commit

--- 2.js
+++ 2.js
@@ -20,7 +20,7 @@ let x = 20;
 x = x => {return x * 20;}
 console.log(x);
 
-var materials = [
+let materials = [
     'Hydrogen',
     'Helium',
     'Lithium',
@@ -36,3 +36,46 @@ var materials = [
   }); // [8, 6, 7, 9]
   
   materials.map(material => material.length); // [8, 6, 7, 9]
+
+  // An empty arrow function returns undefined
+let empty = () => {};
+
+(() => 'foobar')(); 
+// Returns "foobar"
+// (this is an Immediately Invoked Function Expression 
+// see 'IIFE' in glossary)
+
+var simple = a => a > 15 ? 15 : a; 
+simple(16); // 15
+simple(10); // 10
+
+let max = (a, b) => a > b ? a : b;
+
+// Easy array filtering, mapping, ...
+
+var arr = [5, 6, 13, 0, 1, 18, 23];
+
+var sum = arr.reduce((a, b) => a + b);  
+// 66
+
+var even = arr.filter(v => v % 2 == 0); 
+// [6, 0, 18]
+
+var double = arr.map(v => v * 2);       
+// [10, 12, 26, 0, 2, 36, 46]
+
+// More concise promise chains
+promise.then(a => {
+  // ...
+}).then(b => {
+  // ...
+});
+
+// Parameterless arrow functions that are visually easier to parse
+setTimeout( () => {
+  console.log('I happen sooner');
+  setTimeout( () => {
+    // deeper code
+    console.log('I happen later');
+  }, 1);
+}, 1);  
