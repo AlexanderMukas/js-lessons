28ca3e754ba95d2efc464ef94eff7e565dc0b7a9
AlexanderMukas <alexandermukas@yahoo.com>
2020-08-25 23:47:26 +0300

    Some Commit

--- 2.js
+++ 2.js
@@ -12,3 +12,27 @@ let myCar = {
     year: 1995
 };
 console.log(`My car is - ${myCar.brand} ${myCar.model} `);
+
+myFunc(myCar); // rewrite variable brand
+console.log(myCar);
+
+let x = 20;
+x = x => {return x * 20;}
+console.log(x);
+
+var materials = [
+    'Hydrogen',
+    'Helium',
+    'Lithium',
+    'Beryllium'
+  ];
+  
+  materials.map(function(material) { 
+    return material.length; 
+  }); // [8, 6, 7, 9]
+  
+  materials.map((material) => {
+    return material.length;
+  }); // [8, 6, 7, 9]
+  
+  materials.map(material => material.length); // [8, 6, 7, 9]
