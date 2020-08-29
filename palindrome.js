bc86d2841c0db1245a1ad847aacd3a85219a5ad1
AlexanderMukas <alexandermukas@yahoo.com>
2020-08-29 17:25:43 +0300

    Add palindrome file Commit

---
+++ palindrome.js
@@ -0,0 +1,21 @@
+// functions: push, pop, peak, length
+var letters = []; // this is our stack
+
+var word = "Alexander"; // example - racecar
+var rword = "";      // reverseWord
+
+//put letters of word into stack
+for(var i = 0; i < word.length; i++){
+    letters.push(word[i]);
+}
+
+//pop off the stack in reverse order
+for(var i = 0; i <word.length; i++) {
+    rword += letters.pop();
+}
+
+if(rword === word){
+    console.log(`"${word}" is a palidrome word!`);
+} else {
+    console.log(`"${word}" is NOT a palidrome word!`);
+}
