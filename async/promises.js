a2a9c7b9e0e37df9bf47211e82d922b3e4b8e281
AlexanderMukas <alexandermukas@yahoo.com>
2020-09-04 00:06:08 +0300

    add four promise

--- async/promises.js
+++ async/promises.js
@@ -29,10 +29,17 @@ const posts = [
         });
     }
     
-    //getPosts();
     
-    //createPost( {title: 'Post Three', body: 'This is post three'}, getPosts );
-    createPost( {title: 'Post Three', body: 'This is post three'} )
-        .then( getPosts )
-        .catch( (err) => console.log(err));   //catch the error end write in console log
+    //createPost( {title: 'Post Three', body: 'This is post three'} )
+    //    .then( getPosts )
+    //    .catch( (err) => console.log(err));   //catch the error end write in console log
     
+    // Promise.all
+    const promise1 = Promise.resolve('Hello World!');
+    const promise2 = 10;
+    const promise3 = new Promise( (resolve, reject) => 
+        setTimeout(resolve, 2000, 'Goodbye!') );
+    const promise4 = fetch('https://jsonplaceholder.typicode.com/users').then( res => res.json());
+
+    Promise.all( [promise1, promise2, promise3, promise4] )              // array of Promises
+        .then( (values) => console.log(values) );
