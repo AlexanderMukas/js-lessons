3aa3b60870963468aa1ebc46669810267f99147f
AlexanderMukas <alexandermukas@yahoo.com>
2020-09-08 16:45:01 +0300

    opp add file

---
+++ oop.js
@@ -0,0 +1,15 @@
+// oop - class
+
+class Animal {
+    firstName;
+    lastName;
+    constructor(firstName, lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+    };
+
+    sayName{
+        console.log(`${firstName} ${lastName} - good animal`);
+    };
+        
+}
