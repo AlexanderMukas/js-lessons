69e13de77485f98eee72291a0c245804f148595c
AlexanderMukas <alexandermukas@yahoo.com>
2020-09-08 17:55:42 +0300

    some commit

--- oop.js
+++ oop.js
@@ -1,20 +1,22 @@
 // oop - class
-
+"use strict";
 class Person {
     constructor(firstName, lastName){
         this.firstName = firstName;
         this.lastName = lastName;
+        this.count++;
         console.log("Constructor done...")
-    };
+    }
 
     sayName(){
         console.log(`${this.firstName} ${this.lastName} - good person and human!`);
-    };
-    //let firstName;
+    }
+    let this.firstName;
     //let lastName;
 
 }
 
 let obj1 = new Person("Ivan", "Ivanov");
-
+let obj2 = new Person("Peter", "Kawosov");
 obj1.sayName();
+obj2.sayName();
