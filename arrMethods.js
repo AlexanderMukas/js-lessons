ea23e644e41a68e0124fc0fdbb68bd58083e07b8
AlexanderMukas <alexandermukas@yahoo.com>
2020-09-11 00:15:55 +0300

    add new methods commit

--- arrMethods.js
+++ arrMethods.js
@@ -1,3 +1,4 @@
+// Filter, map, find, forEach, some, every, reduce,
 // FILTER METHOD
 const items = [
     {name: 'Bike', price: 100},
@@ -12,11 +13,47 @@ const items = [
 const filteredItems = items.filter( (item)=> {
     return item.price <= 100;
 });
-console.log(items);
-console.log(filteredItems);
+// console.log(items);
+// console.log(filteredItems);
 
 //MAP METHOD
+// Map only names or prices in new list
+const itemNames = items.map( (item)=> {
+    return item.name;
+});
+//console.log(itemNames);
 
-const itemNames = items.map(
-    
-)
+//FIND METHOD
+const foundItem = items.find( (item)=> {
+    return item.name === 'Book';
+});
+//console.log(foundItem);
+
+// FOREACH METHOD (like loop function)
+// items.forEach( (item)=> {
+//     console.log(`${item.name} with price - ${item.price} $`);
+// });
+
+// SOME METHOD
+// return true if find
+const hasInexpensiveItems = items.some( (item)=> {
+    return item.price === 5;
+    //return item.price <= 0;  // false
+});
+// console.log(hasInexpensiveItems); // true
+
+// EVERY METHOD
+// true is every item is frue expression
+const hasInexpensiveItems2 = items.every( (item)=> {
+    return item.price === 200;
+    //return item.price <= 0;  
+});
+// console.log(hasInexpensiveItems2); 
+
+// REDUCE METHOD
+// total price for example
+const total = items.reduce( (currentTotal, item) => {
+    return item.price + currentTotal;
+}, 0);                                    // zero is currentTotal
+
+console.log(total);
