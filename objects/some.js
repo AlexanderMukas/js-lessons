eab1a4f8ee598e75c3e8388790d7da3ffe15f66a
AlexanderMukas <alexandermukas@yahoo.com>
2020-09-25 23:55:59 +0300

    now all ok commit

--- objects/some.js
+++ objects/some.js
@@ -7,11 +7,10 @@ const person = {
         console.log('Greet from person!')
     }
 };
-console.log('Hello!')
-console.log(temp + 'Hm');
+
 person.greet();
 
-const temp = person.languages.array.forEach(lang => {
+person.languages.forEach(lang => {
     console.log(`This person know ${lang}`);
 });
 
