b65e266fec832d3298b601336a914486083f5d11
AlexanderMukas <alexandermukas@yahoo.com>
2020-09-25 23:40:36 +0300

    objects learning commit

---
+++ functions/objects/some.js
@@ -0,0 +1,9 @@
+const person = {
+    name: 'Alexander',
+    age: '31',
+    isProgrammer: true,
+    languages: ['ukrainian', 'russian', 'english'],
+    greet(){
+        console.log('Greet from person')
+    }
+}
