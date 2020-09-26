d0df28b55369a661ef63208114a42e225f275071
AlexanderMukas <alexandermukas@yahoo.com>
2020-09-26 21:41:52 +0300

    learning context commit

--- objects/context.js
+++ objects/context.js
@@ -14,3 +14,17 @@ person.languages.forEach(lang => {
     console.log(`This person know ${lang}`);
 });
 
+const logger = {
+    keys() {
+        console.log('Object Keys:', Object.keys(this) )
+    },
+
+    // keysAndValues() {
+    //     Object.keys(this).forEach( (key) => {
+    //         console.log(`key: ${this[key]}`);
+    //     })
+        
+    // }
+}
+
+logger.keys.call(person);
