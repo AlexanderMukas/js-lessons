0c0f579d93f489a293dca903b615367a5468f854
AlexanderMukas <alexandermukas@yahoo.com>
2020-09-26 22:13:05 +0300

    call method commit

--- objects/context.js
+++ objects/context.js
@@ -8,25 +8,48 @@ const person = {
     }
 };
 
-person.greet();
+// person.greet();
 
-person.languages.forEach(lang => {
-    console.log(`This person know ${lang}`);
-});
+// person.languages.forEach(lang => {
+//     console.log(`This person know ${lang}`);
+// });
 
 const logger = {
     keys() {
         console.log('Object Keys:', Object.keys(this) )
     },
-    
+
     //4:11:28 
     keysAndValues() {
-        Object.keys(this).forEach( (key) => {
-            console.log(`key: ${key}, value: ${this[key]}`);
+        Object.keys(this).forEach( key => {
+            console.log(`${key} : ${this[key]}`);
         })
         
+    },
+    withParams(top = false, between = false, bottom = false) {
+        if(top){
+            console.log('----- START -----');
+        };
+
+        Object.keys(this).forEach( key => {
+            console.log(`${key} : ${this[key]}`);
+
+            if(between){
+                console.log('-----------------');
+            }
+        })
+        if(bottom){
+            console.log('------ END ------');
+        };
+
     }
 }
 
 // logger.keys.call(person);
-logger.keysAndValues.call(person);
+// logger.keysAndValues.call(person);
+let top = true;
+let between = true;
+let bottom = true;
+
+// call(this, param1, param2, param3)
+logger.withParams.call(person, top, between, bottom);
