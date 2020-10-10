db7bf2c9a209367442e9633607fe01128056100c
AlexanderMukas <alexandermukas@yahoo.com>
2020-10-10 16:07:28 +0300

    Constructor commit

--- patterns/1.Creational-Design-Patterns/Constructor.js
+++ patterns/1.Creational-Design-Patterns/Constructor.js
@@ -1,1 +1,17 @@
 // 1. Constructor
+
+//////////////////////////////// es5 //////////////////////////
+// Class implementation
+function Server( name, ip ) {
+    this.name = name
+    this.ip = ip
+}
+
+// create like Class method
+Server.prototype.getUrl = function() {
+    return `https://${this.ip}:80`;
+}
+
+// create new instance object
+const aws = new Server('AWS German', '82.21.21.32');
+console.log( aws.getUrl() );
--- patterns/singleton.js
+++
@@ -1,2 +0,0 @@
-// https://www.youtube.com/watch?v=YJVj4XNASDk&ab_channel=%D0%92%D0%BB%D0%B0%D0%B4%D0%B8%D0%BB%D0%B5%D0%BD%D0%9C%D0%B8%D0%BD%D0%B8%D0%BD
-
