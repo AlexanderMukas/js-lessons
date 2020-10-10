7f0c02a630a4fcf90759300caf220452beda0cac
AlexanderMukas <alexandermukas@yahoo.com>
2020-10-10 20:36:10 +0300

    adapter pattern commit

--- patterns/2.Structural-Design-Patterns/5_Adapter.js
+++ patterns/2.Structural-Design-Patterns/5_Adapter.js
@@ -4,7 +4,7 @@ class OldCalc {
     operations(t1, t2, operation) {
         switch(operation) {
             case 'add': return t1 + t2;      // x+y
-            case 'sub': return t1 + t2;      // x-y
+            case 'sub': return t1 - t2;      // x-y
             default: return NaN;           
         };
     };
@@ -19,3 +19,27 @@ class NewCalc {
         return t1 - t2;
     };
 };
+
+class CalcAdapter {
+    constructor() {
+        this.calc = new NewCalc();
+    }
+
+    operations(t1, t2, operation) {
+        switch(operation) {
+            case 'add': return this.calc.add(t1, t2)      // x+y
+            case 'sub': return this.calc.sub(t1, t2);     // x-y
+            default: return NaN;           
+        };
+    };
+};
+
+const oldCalc = new OldCalc();
+console.log(oldCalc.operations(10, 5, 'add') ); // 15
+
+const newCalc = new NewCalc();
+console.log(newCalc.add(10, 5) );               // 15
+
+const adapter = new CalcAdapter();
+console.log(adapter.operations(25, 10, 'sub') ); // 15
+
