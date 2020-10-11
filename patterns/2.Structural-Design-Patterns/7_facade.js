3ce304869615367289e796477acd50e1943b6363
AlexanderMukas <alexandermukas@yahoo.com>
2020-10-11 23:59:17 +0300

    facade commit

--- patterns/2.Structural-Design-Patterns/7_facade.js
+++ patterns/2.Structural-Design-Patterns/7_facade.js
@@ -1,2 +1,36 @@
 //// 7. Facade
-// very important for JS developers
+// very important for JS developers, like qjuery
+// жалобы = Complains
+class Complains {
+    constructor() {
+        this.complains = [];
+    };
+
+    reply(complaint) { };
+
+    add(complaint){
+        this.complains.push(complaint);
+        return this.reply(complaint);
+    };
+};
+
+class ProductComplaints extends Complains {
+    //override
+    reply( {id, customer, details} ){
+        return `Product: ${id}: ${customer} (${details})`;
+    }
+}
+
+class ServiceComplaints extends Complains { 
+    //override
+    reply( {id, customer, details} ){
+        return `Service: ${id}: ${customer} (${details})`;
+    }
+}
+
+// FACADE CLASS:
+class ComplaintRegistry {
+    register(customer, type, details) {
+
+    }
+}
