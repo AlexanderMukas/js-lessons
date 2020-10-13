c875178b862697f23d6a9edd8840b07c97854eb7
AlexanderMukas <alexandermukas@yahoo.com>
2020-10-13 23:53:37 +0300

    finally flyweight commit

--- patterns/2.Structural-Design-Patterns/8_Flyweight.js
+++ patterns/2.Structural-Design-Patterns/8_Flyweight.js
@@ -1,4 +1,5 @@
 //// 8. Flyweight
+// use for cache 
 
 class Car {
     constructor(model, price) {
@@ -14,9 +15,29 @@ class CarFactory {
 
     create(model, price) {
         const candidate = this.getCar(model);
+        if(candidate) {
+            return candidate;
+        }
+
+        const newCar = new Car(model, price);
+        this.cars.push(newCar);
+
+        return newCar;
+        
     }
 
     getCar(model){
         return this.cars.find( car => car.model === model);
     }
 }
+
+
+const factory = new CarFactory();
+
+const bmwX6 = factory.create('bmw', 10000);
+const bmwX3 = factory.create('bmw', 6000);
+console.log(bmwX6);
+console.log(bmwX3);
+
+const audi = factory.create('audi', 8000);
+console.log(audi);
