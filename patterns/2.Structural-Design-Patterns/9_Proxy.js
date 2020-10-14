39fd9344bef51d11bae5400127c406c6cec31858
AlexanderMukas <alexandermukas@yahoo.com>
2020-10-14 18:49:06 +0300

    proxy pattern commit

--- patterns/2.Structural-Design-Patterns/9_Proxy.js
+++ patterns/2.Structural-Design-Patterns/9_Proxy.js
@@ -1,2 +1,27 @@
 //// 9. Proxy
 // very important for JS
+
+function networkFetch(url) {
+    return `${url} - Answer from server`;
+}
+
+const cache = new Set();       // uniq
+
+const proxiedFetch = new Proxy(networkFetch, {
+    apply(target, thisArg, args) {
+        const url = args[0];   // get first arg of function
+
+        if( cache.has(url) ) {
+            return `${url} - *Answer from cache*`
+        } else {
+            cache.add(url);
+            return Reflect.apply(target, thisArg, args);
+        };
+    }
+}); 
+
+console.log(  proxiedFetch('argular.io')   );
+console.log(  proxiedFetch('react.io')   );
+console.log(  proxiedFetch('argular.io')   );
+
+
