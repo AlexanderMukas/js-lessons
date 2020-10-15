b94a1f012fab71e5886b1d3653af20cf1b0dd0a5
AlexanderMukas <alexandermukas@yahoo.com>
2020-10-15 23:35:51 +0300

    some commit

--- patterns/3.Behavioral-Design-Patterns/13_Mediator.js
+++ patterns/3.Behavioral-Design-Patterns/13_Mediator.js
@@ -1,1 +1,42 @@
 // 13. Mediator 
+// new users in chat without socket
+
+class User {
+    constructor(name) {
+        this.name = name;
+        this.room = null;
+    }
+
+    send(message, to) {
+        this.room.send(message, this, to);
+
+    }
+
+    receive(message, from) {
+        console.log(`${from.name} => ${this.name}: ${message}`);
+    }
+}
+
+
+class ChatRoom{
+    constructor(){
+        this.users = {}
+    }
+
+    register(user) {
+        this.users[user.name] = user;
+        user.room = this;
+    }
+
+    send(message, from, to) {
+        if(to) {
+            to.receive(message, from);
+        } else {
+            Object.keys(this.users).forEach( key => {
+                if(this.users[key] !== from) {
+                    this.users[key].receive(message, from)
+                }
+            })
+        }
+    }
+}
