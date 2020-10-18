41fdbb8915edff6303920ee7488339030f48f48f
AlexanderMukas <alexandermukas@yahoo.com>
2020-10-18 23:45:22 +0300

    opp commit

--- oop.js
+++ oop.js
@@ -16,3 +16,20 @@ let obj1 = new Person("Ivan", "Ivanov");
 let obj2 = new Person("Peter", "Kawosov");
 obj1.sayName();
 obj2.sayName();
+
+class Animal {
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
+
+    walk(){
+        console.log(`animal ${this.name} go walking in area. Age: ${this.age}`);
+    };
+
+    talk() {};
+}
+
+class Cat extends Animal {
+    constructor(name, age) 
+}
